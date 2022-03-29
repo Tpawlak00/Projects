af54f95f584b628b0a7fecaee34d27161cd5528d
Tpawlak00 <94640038+Tpawlak00@users.noreply.github.com>
2022-03-29 14:22:03 +0200

    another one

--- Cryptocurrency Forecasting/future_predictions.py
+++ Cryptocurrency Forecasting/future_predictions.py
@@ -17,36 +17,55 @@ df = pd.read_csv('./data/LTC_DATA/DailyData/Binance_LTCUSDT_d3.csv', index_col='
 df.drop(['symbol'], inplace=True, axis=1)
 df = df.astype('float')
 
-values = df[0:6*pred_len][['close']].astype(float)
-values.rename(columns={'close': 'Real Values'}, inplace=True)
-data_test = df.loc['2021':]['close']
+data_test = df.loc['2021-01-01':'2022-01-01']['close']
 data_test = np.array(data_test)
 
 model = tf.keras.models.load_model('saved_model/MODEL1')
 x_test, y_test = [], []
 for i in range(pred_len, len(data_test)):
     x_test.append(data_test[i-pred_len:i])
-    y_test.append(data_test[i:i + pred_len])
 
 print(pd.DataFrame(x_test))
-print(pd.DataFrame(y_test))
 x_test = np.array(x_test)
 x_test = x_scaler.fit_transform(x_test)
 x_test = x_test.reshape(len(x_test[:]), len(x_test[0]), 1)
 
+# making test predictions to see how the models works
 y_pred = model.predict(x_test)
 y_pred = x_scaler.inverse_transform(y_pred)
-pred = y_pred[:, pred_len-1]
-print(y_pred)
-add_dates = [df.index[0]+DateOffset(days=x) for x in range(0, pred_len+1)]
-future_dates = pd.DataFrame(index=add_dates[1:], columns=df.columns)
-df_predict = pd.DataFrame(y_pred[0:pred_len,0],
-                          index=future_dates[:pred_len].index,
-                          columns=['Future Prediction'])
-values = df[0:len(pred)][['close']].astype(float)
-values['Test Prediction'] = pred
-df_plot = pd.concat([values, df_predict], axis=1)
-print(df_plot.tail(20))
-df_plot.rename(columns={'close': 'Real Values'}, inplace=True)
-df_plot.plot(figsize=(14, 5))
+test_pred = y_pred[::-1, 0]
+
+
+# making future predictions by predicting a few times
+future_pred = []
+for i in range(0, pred_len):
+    y_pred = x_scaler.transform(y_pred)
+    y_pred = model.predict(y_pred)
+    y_pred = x_scaler.inverse_transform(y_pred)
+    for j in range(0, pred_len):
+        future_pred.append(y_pred[-len(y_pred[::-1, 0])+pred_len-j, 0])
+
+print(len(test_pred))
+print(test_pred)
+print(len(future_pred))
+print(future_pred)
+
+values = df['2021-01-01':'2022-03-22'][['close']].astype(float)
+
+# making array of test values
+add_dates = [values.index[-pred_len]+DateOffset(days=x) for x in range(0, len(test_pred)+1)]
+dates = pd.DataFrame(index=add_dates[1:], columns=df.columns)
+predict = pd.DataFrame(test_pred, index=dates.index, columns=['Test Prediction'])
+
+# making array of future values
+add_future_dates = [predict.index[-1]+DateOffset(days=x) for x in range(0, len(future_pred)+1)]
+future_dates = pd.DataFrame(index=add_future_dates[1:], columns=df.columns)
+df_predict = pd.DataFrame(future_pred, index=future_dates.index, columns=['Future Prediction'])
+
+values.rename(columns={'close': 'Real Values'}, inplace=True)
+values['Test Prediction'] = predict
+values['Future Prediction'] = df_predict
+
+values.rename(columns={'close': 'Real Values'}, inplace=True)
+values.plot(figsize=(14, 5))
 plt.show()
